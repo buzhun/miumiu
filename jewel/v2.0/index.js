15b17f96c805bc708d26654f152f153400efff6f
wanglinlin01 <wanglinlin01@sogou-inc.com>
2020-10-16 11:00:30 +0800

    非递归实现

--- jewel/v2.0/index.js
+++ jewel/v2.0/index.js
@@ -1,26 +1,43 @@
-// 动态规划，遍历前一个得到下一个
-function getMax(list) {
-  var max = 0;
-  var lasti = -1;
-  function getSum(i) {
-    if (i === 0) {
-      lasti = -1;
-      return list[0];
-    }
-    var lastMax = getSum(i - 1);
+//  动态规划，遍历前一个得到下一个
+// 递归
+// function getMax(list) {
+//   var max = 0;
+//   var lasti = -1;
+//   function getSum(i) {
+//     if (i === 0) {
+//       lasti = -1;
+//       return list[0];
+//     }
+//     var lastMax = getSum(i - 1);
 
-    var result = Math.max(lastMax + list[i], list[i]);
-    if (result > max) {
-      max = result;
-      duration = [lasti, i]; // 当前这个是右边
-    } else {
-      lasti = i + 1; // 这个不是，要从下一个开始。
+//     var result = Math.max(lastMax + list[i], list[i]);
+//     if (result > max) {
+//       max = result;
+//       duration = [lasti, i]; // 当前这个是右边
+//     } else {
+//       lasti = i + 1; // 这个不是，要从下一个开始。
+//     }
+//     return result;
+//   }
+//   getSum(list.length - 1);
+//   return { max, duration };
+// }
+
+// 后面的可以根据前面的推导出来
+function getMaxDurationSum(list) {
+  var f = [];
+  var max = 0;
+  for (var i = 0; i < list.length; i++) {
+    f[i] = list[0];
+    if (i >= 1) {
+      f[i] = Math.max(f[i - 1] + list[i], list[i]);
     }
-    return result;
+    if (f[i] > max) {
+      max = f[i];
+    }
   }
-  getSum(list.length - 1);
-  return { max, duration };
+  return max;
 }
 
 var list = [1, -3, 24, 3, 45, -99, 6, 3, 10];
-console.log(getMax(list));
+console.log(getMaxDurationSum(list));
